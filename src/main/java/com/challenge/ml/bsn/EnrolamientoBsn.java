cd32986b46e0f9a5032773bb5e9a7bf333acb5b2
gtapiad <gtapiad@EKT1673332LAP.ad.elektra.com.mx>
2021-11-18 18:01:01 -0600

    getToke en buisness

--- src/main/java/com/challenge/ml/bsn/EnrolamientoBsn.java
+++ src/main/java/com/challenge/ml/bsn/EnrolamientoBsn.java
@@ -1,5 +1,7 @@
 package com.challenge.ml.bsn;
 
 public interface EnrolamientoBsn {
+	
+	String getJWTToken(String userName);
 
 }
--- src/main/java/com/challenge/ml/bsn/EnrolamientoBsnImpl.java
+++ src/main/java/com/challenge/ml/bsn/EnrolamientoBsnImpl.java
@@ -1,11 +1,43 @@
 package com.challenge.ml.bsn;
 
 
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 
 @Service
 public class EnrolamientoBsnImpl implements EnrolamientoBsn {
+
+	@Override
+	public String getJWTToken(String userName) {
+		String secretKey = "llaveSecreta";
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList("ROLE_USER");
+		
+		String token = Jwts
+				.builder()
+				.setId("jwtChallenge")
+				.setSubject(userName)
+				.claim("authorities",
+						grantedAuthorities.stream()
+								.map(GrantedAuthority::getAuthority)
+								.collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.signWith(SignatureAlgorithm.HS512,
+						secretKey.getBytes()).compact();
+
+		return "Bearer " + token;
+	}
+
 	
 
 }
