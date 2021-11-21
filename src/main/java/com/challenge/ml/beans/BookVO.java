acb2a26ea1d6a45152b46d67af8a6eb44503e7a4
gtapiad <gtapiad@EKT1673332LAP.ad.elektra.com.mx>
2021-11-21 02:54:22 -0600

    rest template

--- src/main/java/com/challenge/ml/beans/BookVO.java
+++ src/main/java/com/challenge/ml/beans/BookVO.java
@@ -1,5 +1,12 @@
 package com.challenge.ml.beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookVO {
 
 	private int idBook;
@@ -8,36 +15,7 @@ public class BookVO {
 	private String title;
 	private String publisher;
 	
-	public int getIdBook() {
-		return idBook;
-	}
-	public void setIdBook(int idBook) {
-		this.idBook = idBook;
-	}
-	public String getIdGoogle() {
-		return idGoogle;
-	}
-	public void setIdGoogle(String idGoogle) {
-		this.idGoogle = idGoogle;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getPublisher() {
-		return publisher;
-	}
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
+	
 	@Override
 	public String toString() {
 		return "BookVO [idBook=" + idBook + ", idGoogle=" + idGoogle + ", author=" + author + ", title=" + title
