be700ec36b5df24716e1342dbf144f25ed412509
erick.garcia <erick.garbed.isc@gmail.com>
2021-11-21 21:12:14 -0600

    change in rest client.

--- src/main/java/com/challenge/ml/controller/GoogleBooksController.java
+++ src/main/java/com/challenge/ml/controller/GoogleBooksController.java
@@ -1,36 +1,34 @@
 package com.challenge.ml.controller;
 
 
-import java.util.Arrays;
-
-import org.springframework.http.HttpHeaders;
+import com.challenge.ml.api.books.model.Wrapper;
+import com.challenge.ml.beans.GoogleBooksVO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.challenge.ml.api.books.model.Wrapper;
-import com.challenge.ml.beans.GoogleBooksVO;
-
 @RestController
 public class GoogleBooksController {
-	
 
-	
-	@GetMapping("/getBooks")
-	ResponseEntity<ResponseEntity<Wrapper>> getBooks(@RequestBody GoogleBooksVO googleBooksVO) {
-		  System.out.println(googleBooksVO.toString());
-		  //GoogleBooksVO result=googleBooksBsn.getBooks(googleBooksVO);
-	      HttpHeaders headers = new HttpHeaders();
-	      RestTemplate restTemplate=new RestTemplate();
-	      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	      System.out.println("Entra books");
-	      ResponseEntity response = restTemplate.getForEntity("https://www.googleapis.com/books/v1/volumes?q="+googleBooksVO.getTitle(),Wrapper.class);	
-	      System.out.println("Consumio template");
-	      System.out.println(response.getBody());
-		  return ResponseEntity.status(HttpStatus.OK).body(response);
-	   }
+	@Value("${google.books.url}")
+	private String url;
+
+    @PostMapping("/findBooks")
+    ResponseEntity<Wrapper> getBooks(@RequestBody GoogleBooksVO googleBooksVO) {
+        System.out.println(googleBooksVO.toString());
+        //GoogleBooksVO result=googleBooksBsn.getBooks(googleBooksVO);
+        RestTemplate restTemplate = new RestTemplate();
+        System.out.println("Entra books");
+        ResponseEntity<Wrapper> response = restTemplate.getForEntity(url + googleBooksVO.getTitle(), Wrapper.class);
+        if (response.getStatusCode().value() == 200) {
+            System.out.println("Consumio template");
+            return ResponseEntity.status(HttpStatus.OK).body(response.getBody());
+        } else {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
 }
